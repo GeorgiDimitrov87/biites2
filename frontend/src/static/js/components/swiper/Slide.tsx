ebd79020d1a69031953ee5a82e919bb9d0d3248c
dev_hunter <48398993+decovicdev@users.noreply.github.com>
2022-02-16 21:24:46 +0800

    Update Slide.tsx

--- frontend/src/static/js/components/swiper/Slide.tsx
+++ frontend/src/static/js/components/swiper/Slide.tsx
@@ -1,13 +1,10 @@
 import Reac from 'react';
 import { SwiperSlide, SwiperSlideProps } from "swiper/react";
 
-import styles from "./Swiper.module.scss";
+import "./swiper.scss";
 
-const Slide: React.FC<SwiperSlideProps> = (props) => {
-  const { className, ...rest } = props;
-  const classes = [styles.slide, className].join(" ");
-
-  return <SwiperSlide className={classes} {...rest} />;
+const Slide: React.FC<SwiperSlideProps> = (props) => { 
+  return <SwiperSlide  {...props} />;
 };
 
 Slide.displayName = "SwiperSlide";
